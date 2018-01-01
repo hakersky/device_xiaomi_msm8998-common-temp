41d387312ad23ce1ac5907f2478469561af7a928
LuK1337 <priv.luk@gmail.com>
2018-01-01 23:10:58 +0100

    msm8998-common: Implement on boot TFA98xx calibration [2/2]
    * TFA amplifier requires to play a calibration
      sound file on each boot otherwise it calibrates
      on the first played sound eg: unlock phone sound.
    
    Change-Id: Ice91c29d14e30ed688007cffff56375b178106c2

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -92,6 +92,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tfa.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.tfa.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.panel_info.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
